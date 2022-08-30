e23194b491402a7695f87145f9d61f92e4fcbe90
caren <carenoliveirasilvapontes@gmail.com>
2022-08-30 15:35:12 -0500

    requisito 1

---
+++ src/pages/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (<div data-testid="page-album"><p>Album</p></div>);
+  }
+}
+
+export default Album;
---
+++ src/pages/Favorites.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Favorites extends React.Component {
+  render() {
+    return (<div data-testid="page-favorites"><p>Favorites</p></div>);
+  }
+}
+
+export default Favorites;
