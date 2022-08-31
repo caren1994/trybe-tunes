75e6df0c2f21272301a61e82df47e6ac53f1c932
caren <carenoliveirasilvapontes@gmail.com>
2022-08-30 20:05:42 -0500

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,55 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    nome: '',
+    isDisabled: true,
+  };
+
+  handleChange = (event) => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      },
+      () => {
+        const { nome } = this.state;
+        const number2 = 2;
+        if (nome.length >= number2) {
+          this.setState({
+            isDisabled: false,
+          });
+        } else {
+          this.setState({
+            isDisabled: true,
+          });
+        }
+      },
+    );
+  };
+
   render() {
+    const { nome, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
-        <p>Search</p>
+        <form>
+          <label htmlFor="name">
+            Procurar:
+            <textarea
+              onChange={ this.handleChange }
+              name="nome"
+              value={ nome }
+              data-testid="search-artist-input"
+            />
+          </label>
+          <button
+            type="button"
+            //   onClick={ this. }
+            disabled={ isDisabled }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
         <Header />
       </div>
     );
