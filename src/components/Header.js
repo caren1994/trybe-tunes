fef92a06ce84fcbb6105fe00e28b17736adcf2cd
caren <carenoliveirasilvapontes@gmail.com>
2022-08-30 19:49:31 -0500

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 import Loading from '../pages/Loading';
@@ -24,6 +25,15 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         {loading ? <Loading /> : <h1 data-testid="header-user-name">{user}</h1>}
+        <Link to="/search" data-testid="link-to-search">
+          Search
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favorites
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          Profile
+        </Link>
       </header>
     );
   }
