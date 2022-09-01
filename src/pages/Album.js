3cb6c2e24664f1ccbc5bf1b1dbffb37105324b24
caren <carenoliveirasilvapontes@gmail.com>
2022-09-01 17:11:35 -0500

    requito 11

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
-import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Loading from './Loading';
 
 class Album extends React.Component {
@@ -15,27 +15,36 @@ class Album extends React.Component {
   };
 
   async componentDidMount() {
-    await this.favorite();
-    const { match: { params: { id } } } = this.props;
-    const musicas = await getMusics(id);
+    await this.favorite();// chamo antes de redenzirar o componente para saber se tem algo no local
+    const { match: { params: { id } } } = this.props;// pego o id do album
+    const musicas = await getMusics(id);// faz a requisição a api e retorna as musicas do album recebe como argumento o id passado na rota album:id
     this.setState({
-      result: musicas,
+      result: musicas, // guardo as musicas e as informaçoes do cantor ou banda no estado result
 
     });
   }
 
-  createFavorite = async (e) => {
-    this.setState({ loading: true }, async () => {
-      await addSong(e);
-      await this.favorite();
-      this.setState({
-        loading: false });
-    });
+  createFavorite = async (element) => {
+    const { favoritos } = this.state;
+    const novamusica = favoritos.some((e) => e.trackName === element.trackName);
+    if (novamusica) {
+      this.setState({ loading: true }, async () => {
+        await removeSong(element);// removo a musica
+        await this.favorite();// atualizo o array com a musica favoritada removida
+        this.setState({ loading: false });
+      });
+    } else {
+      this.setState({ loading: true }, async () => { // muda o loading para true e lá embaixo no render mostra o componente Loading
+        await addSong(element);// adc no local story
+        await this.favorite();// chamo a função de pegar os itens,após ser salvo
+        this.setState({ loading: false });// retorno para falso
+      });
+    }
   };
 
   favorite = async () => {
-    const checked = await getFavoriteSongs();
-    this.setState({ favoritos: checked });
+    const pegalocal = await getFavoriteSongs();// pego do local story
+    this.setState({ favoritos: pegalocal });// favoritos recebe os dados do local
   };
 
   render() {
